ba1c9b77be1ba42134084b2b93806cc40a0143f8
Barry-Allen8 <127853019+Barry-Allen8@users.noreply.github.com>
2026-02-09 14:55:42 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "biuro-rachunkowe-ksiegowa.vercel.app",
       },
+      {
+        protocol: "https",
+        hostname: "karpol.vercel.app",
+      },
     ],
   },
 };
